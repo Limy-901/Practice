a11bd3a9fefc961556868e188a23214baec6eedb
Limy <ly.misty901@gmail.com>
2022-05-30 17:30:43 +0900

    limy / last update

--- Android_limy_app/Data/src/main/java/com/example/data/remote/model/DataLoveResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.data.remote.model
-
-data class DataLoveResponse(
-    val fname : String,
-    val sname : String,
-    val percentage : String,
-    val result : String
-)
--- Android_limy_app/Data/src/main/java/com/example/data/repository/MainRepositoryImpl.kt
+++ Android_limy_app/Data/src/main/java/com/example/data/repository/MainRepositoryImpl.kt
@@ -1,25 +1,9 @@
 package com.example.data.repository
 
-import com.example.data.mapper.MainMapper
 import com.example.data.repository.remote.datasource.MainDataSource
-import com.example.domain.model.DomainLoveResponse
 import com.example.domain.repository.MainRepository
-import com.example.domain.utils.RemoteErrorEmitter
 import javax.inject.Inject
 
-class MainRepositoryImpl @Inject constructor(
-    private val mainDataSource: MainDataSource
-) : MainRepository {
+class MainRepositoryImpl {
 
-    override suspend fun checkLoveCalculator(
-        remoteErrorEmitter: RemoteErrorEmitter,
-        host: String,
-        key: String,
-        wName: String,
-        mName: String
-    ): DomainLoveResponse? {
-        // Mapper 를 쓰는 이유?
-        // Data 에는 Domain 과 연결되어 있지 않아서, Mapper 통해서 연결 시켜줌
-        return MainMapper.loveMapper(mainDataSource.checkLoveCalculator(remoteErrorEmitter,host, key, wName, mName))
-    }
 }
