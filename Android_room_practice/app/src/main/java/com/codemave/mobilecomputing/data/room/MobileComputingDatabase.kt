ee83c6f7cf2b2e19da6fa2d0c3c0d36a43a83987
limy <ly.misty901@gmail.com>
2022-05-31 00:51:42 +0900

    limy / add comment

--- Android_room_practice/app/src/main/java/com/codemave/mobilecomputing/data/room/MobileComputingDatabase.kt
+++ Android_room_practice/app/src/main/java/com/codemave/mobilecomputing/data/room/MobileComputingDatabase.kt
@@ -12,6 +12,13 @@ import com.codemave.mobilecomputing.data.entity.Category
     version = 1,
     exportSchema = false
 )
+/**
+ * 룸 데이터 베이스를 상속받는 데이터베이스를 생성
+ */
+
 abstract class MobileComputingDatabase : RoomDatabase() {
+    /**
+     * 이 데이터 베이스는 아래의 펑션을 호출함 (dao)
+     */
     abstract fun categoryDao(): CategoryDao
 }
