6e6b31aaac9d1a8351adaab841d08ef0431a4985
limy <ly.misty901@gmail.com>
2022-06-01 13:12:42 +0900

    limy / old version project to new version project

--- Android/room_practice/app/src/main/java/com/codemave/mobilecomputing/data/entity/Payment.kt
+++
@@ -1,9 +0,0 @@
-package com.codemave.mobilecomputing.data.entity
-
-
-data class Payment(
-    val paymentId: Long,
-    val paymentTitle: String,
-    val paymentDate: Long,
-    val paymentCategoryId: Long
-)
